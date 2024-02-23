757c1479cca13d21f57b546b3742f1cc05fd28d8
Karoline de Souza Quetz <kquetz@viceri.com.br>
2024-02-23 17:01:09 -0300

    feat: criada a biblioteca

---
+++ projects/waycarbon-lib/src/lib/waycarbon-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-waycarbon-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      waycarbon-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class WaycarbonLibComponent {
+
+}
