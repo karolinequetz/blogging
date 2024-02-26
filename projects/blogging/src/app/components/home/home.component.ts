654f0eee4ae879ebe9a4885adf5fa97303bfd9d8
Karoline de Souza Quetz <kquetz@viceri.com.br>
2024-02-26 01:20:38 -0300

    feat: criado componente post

--- projects/blogging/src/app/components/home/home.component.ts
+++ projects/blogging/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, PostComponent],
 })
 export class HomeComponent {}
