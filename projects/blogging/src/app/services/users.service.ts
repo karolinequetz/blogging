69d06ac80ba66a14b8b0831394d9f7e8c26b2917
Karoline de Souza Quetz <kquetz@viceri.com.br>
2024-02-27 19:15:20 -0300

    feat: criado servico para obter os usuários

---
+++ projects/blogging/src/app/services/users.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private url = environment.api;
+  constructor(private http: HttpClient) {}
+
+  getUserById(id: string) {
+    return this.http.get<User>(`${this.url}/users/${id}`);
+  }
+}
