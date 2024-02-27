a4a58ea61ddd09f850e51d400abc027c860554fa
Karoline de Souza Quetz <kquetz@viceri.com.br>
2024-02-27 11:03:45 -0300

    feat: criado componente button na lib waycarbon

--- projects/blogging/src/app/components/comment/comment.component.ts
+++ projects/blogging/src/app/components/comment/comment.component.ts
@@ -2,14 +2,15 @@ import { Comment, ShowedComment } from './../../interfaces/post.interface';
 import { Component, Input, LOCALE_ID } from '@angular/core';
 
 import { CommonModule, DatePipe } from '@angular/common';
+import { ButtonComponent } from '../../../../../waycarbon-lib/src/lib/components/button/button.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
   providers: [DatePipe, { provide: LOCALE_ID, useValue: 'pt' }],
+  imports: [CommonModule, ButtonComponent],
 })
 export class CommentComponent {
   @Input() comments: Comment[] = [];
